027fe73c360680d37ed228bf20dcec402b1e48db
hayato.nishimura <fakstopi@gmail.com>
2020-09-29 14:37:03 +0900

    Create UserModel

--- src/models/User.java
+++ src/models/User.java
@@ -1,7 +1,5 @@
 package models;
 
-import java.sql.Timestamp;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,25 +8,23 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "messages")
+@Table(name = "user")
 public class User {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "title", length = 255, nullable = false)
-    private String title;
+    @Column(name = "name", length = 255, nullable = false)
+    private String name;
 
-    @Column(name = "content", length = 255, nullable = false)
-    private String content;
-
-    @Column(name = "created_at", nullable = false)
-    private Timestamp created_at;
-
-    @Column(name = "updated_at", nullable = false)
-    private Timestamp updated_at;
-
+    @Column(name = "prefectures", length = 255, nullable = false)
+    private String prefectures;
+    
+    @Column(name = "age", nullable = false)
+    private Integer age;
+    
+    
     public Integer getId() {
         return id;
     }
@@ -37,35 +33,28 @@ public class User {
         this.id = id;
     }
 
-    public String getTitle() {
-        return title;
+    public String getName() {
+        return name;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getContent() {
-        return content;
+    public String getPrefectures() {
+        return prefectures;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setPrefectures(String prefectures) {
+        this.prefectures = prefectures;
+    }
+    
+    public int getAge() {
+        return this.age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
     }
 
-    public Timestamp getCreated_at() {
-        return created_at;
-    }
-
-    public void setCreated_at(Timestamp created_at) {
-        this.created_at = created_at;
-    }
-
-    public Timestamp getUpdated_at() {
-        return updated_at;
-    }
-
-    public void setUpdated_at(Timestamp updated_at) {
-        this.updated_at = updated_at;
-    }
 }
